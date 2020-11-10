59b977e6b2204a7bc379c48960a30f07f06f11c0
Brituy <brituy@mail.ru>
2020-11-10 13:34:01 +0300

    db_schema.xml

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
-  \Magento\Framework\Component\ComponentRegistrar::MODULE,
-  'Brituy_SimpleBlog',
-  __DIR__
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'SimpleBlog',
+    __DIR__
 );
